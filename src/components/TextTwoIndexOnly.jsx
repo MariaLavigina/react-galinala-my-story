f7d5111a5c74288c1094a933785cefe674693765
Maria Lavigina <135663272+MariaLavigina@users.noreply.github.com>
2025-10-29 21:10:00 +0000

    other svg added

---
+++ src/components/TextTwoIndexOnly.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TextTwoIndexOnly = () => (
+  <h2 >
+    Мы имеем возможность, именно возможность и уже наше дело воспользоваться ею или нет.
+  </h2>
+);
+
+export default TextTwoIndexOnly;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import TextOneOnly from "../components/TextOneOnly.jsx";
+import TextTwoIndexOnly from "../components/TextTwoIndexOnly.jsx";
+import TextThreeIndexOnly from "../components/TextThreeIndexOnly.jsx";
+
 
 const Index = () => {
   return (
@@ -13,24 +16,51 @@ const Index = () => {
         />
         <div className="relative w-full -mt-[300px]">
           <img
-            src="/images/desktop-images/first-wave-desktop.svg"
+            src="/images/desktop-images/wave-index-entro.svg"
             alt="Wave"
             className="w-full h-auto block"
           />
 <div
   className="
     absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white
-    [&>h1]:font-rubik [&>h1]:italic [&>h1]:font-normal [&>h1]:text-[5vw]
-    [&>h2]:font-playfair [&>h2]:italic [&>h2]:font-normal [&>h2]:text-[8vw]
-    [&>h3]:font-playfair [&>h3]:italic [&>h3]:font-normal [&>h3]:text-[2vw] [&>h3]:mt-8
-    [&>p]:font-roboto [&>p]:font-normal [&>p]:not-italic [&>p]:normal-case [&>p]:text-[1.25vw] [&>p]:mt-6
-    md:[&>p]:text-[1.38vw] lg:[&>p]:text-[1.55vw]
+     [&>h1]:text-[4.5vw]
+     [&>h2]:text-[10vw]
+     [&>h3]:text-[2.5vw] [&>h3]:mt-10 [&>h3]:mb-16
+     [&>p]:text-[1.25vw] [&>p]:mt-6
+     md:[&>p]:text-[1.38vw] lg:[&>p]:text-[1.55vw]
   "
 >
   <TextOneOnly />
 </div>
-        </div>
-      </div>
+</div>
+
+
+         {/* Second text under SVG */}
+    <div className="text-center text-black mt-8 px-4 max-w-[720px] mx-auto [&>h2]:text-[3vw]">
+      <TextTwoIndexOnly />
+    </div>
+      {/* Second text under SVG */}
+
+
+
+<div className="relative w-full hidden lg:block mt-8">
+  <img
+    src="/images/desktop-images/fff.svg"
+    alt="Wave Two"
+    className="w-full h-auto block"
+  />
+  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+    <div className="    [&>h1]:text-[5vw] [&>h1]:mt-8
+                        [&>h2]:text-[2.5vw] [&>h2]:mt-8
+                        [&>p]:text-[1.5vw] [&>p]:mt-8">
+      <TextThreeIndexOnly />
+    </div>
+  </div>
+</div>
+</div>
+      
+
+
       {/* -------- end of desktop-only hero -------- */}
 
       {/* ---------- Tablet-only Hero section ---------- */}
@@ -48,8 +78,8 @@ const Index = () => {
           />
           <div
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white 
-                       w-[90%] sm:w-[95%] px-2 sm:px-4 
-                       [&>h1]:text-[6.5vw] [&>h2]:text-[14vw] [&>h3]:text-[4.5vw] [&>h3]:mt-8 
+                       w-[85%]  px-2 sm:px-4 
+                       [&>h1]:text-[5vw] [&>h2]:text-[11vw] [&>h3]:text-[3.5vw] [&>h3]:mt-8 [&>h3]:mb-12 
                        [&>p]:text-[2vw] [&>p]:mt-6 leading-relaxed"
     
           >
@@ -68,15 +98,17 @@ const Index = () => {
         />
         <div className="relative w-full -mt-[100px]">
           <img
-            src="/images/mobile-images/firstPage-wave-mobile.svg"
+            src="/images/mobile-images/wave-index-entro-mobile.svg"
             alt="Wave"
             className="w-full h-auto block"
           />
           <div
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white 
                        w-[95%] sm:w-[98%] 
-                       [&>h1]:text-[30px] [&>h2]:text-[65px] [&>h3]:text-[22px] [&>h3]:mt-12 
-                       [&>p]:text-[5vw] [&>p]:mt-6 leading-relaxed"
+                       [&>h1]:text-[clamp(23px,8vw,80px)]
+                       [&>h2]:text-[clamp(65px,14vw,200px)]
+                       [&>h3]:text-[clamp(23px,6vw,35px)] [&>h3]:mt-16 [&>h3]:mb-16
+                       [&>p]:text-[clamp(16px,5vw,25px)] [&>p]:mt-8"
      
           >
             <TextOneOnly />
@@ -84,6 +116,8 @@ const Index = () => {
         </div>
       </div>
       {/* -------- end of mobile-only hero -------- */}
+
+
 
       {/* ---------- Text section below SVG (same for all) ---------- */}
       <section className="bg-white px-[16px] sm:px-[24px] md:px-[32px] mt-[48px]">
